ce3720f68bd1cc81e34ac19bcbfb75b3379893d4
mohamedcharfeddine <mohamed.charfeddine@polytechnicien.tn>
2023-05-24 15:39:39 -0700

    first commit

---
+++ taskSchema.js
@@ -0,0 +1,21 @@
+// taskSchema.js
+const { buildSchema } = require("graphql");
+const taskSchema = buildSchema(`
+type Task {
+id: ID!
+title: String!
+description: String!
+completed: Boolean!
+duration: Int!
+}
+type Query {
+task(id: ID!): Task
+tasks: [Task]
+}
+type Mutation {
+addTask(title: String!, description: String!, completed: Boolean!, duration: Int!): Task
+completeTask(id: ID!): Task
+changeDescription(id: ID!, description: String!): Task
+}
+`);
+module.exports = taskSchema;
